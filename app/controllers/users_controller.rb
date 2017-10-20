2e7bbe5464d4481016e7a01fee0d57193508279e
Agustin Pazos <agustin.pazosm@gmail.com>
2017-10-20 21:19:16 +0000

    Imagen en usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :lastName, :email, :document, :image)
+      
+      params.require(:user).permit(:name, :lastName, :email, :document, :image,:avatar)
     end
 end
