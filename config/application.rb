9f60cc70d1d8a9276f2558ba4616f3693b4dfc3d
Martin Long <martin.long.gz@gmail.com>
2017-10-20 23:26:08 +0000

    Se agrega bootstrap y pagina login de usuario

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
+    
+    Rails.application.config.assets.precompile += ['users/login.js', 'users/login.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
