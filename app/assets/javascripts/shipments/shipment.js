75d732104c9791e538bd9409296b5ba00afcc582
Agustin Pazos <agustin.pazosm@gmail.com>
2017-10-28 07:00:57 +0000

    Autocompletar,falta front

--- app/assets/javascripts/shipments/shipment.js
+++ app/assets/javascripts/shipments/shipment.js
@@ -3,7 +3,14 @@ Map.latitude = -34.8804869;
 Map.longitude = -56.1341587;
 Map.markers = [];
 $(document).ready(function () {
-    $('#')
+    $('#shipment_receiver_email').on('keyup',function(e){
+        $(this).css("background","#FFF url(/images/LoaderIcon.gif) no-repeat 165px");
+
+        delay(function(){
+            searchUser($('#shipment_receiver_email').val());
+        }, 200);
+
+    });
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
         draggable: true,
@@ -37,3 +44,45 @@ $(document).ready(function () {
     
 
 });
+
+function searchUser(text){
+    var myObject = new Object();
+    myObject.receiverEmail = text;
+    $.ajax({
+            type: "POST", 
+            url: "https://enviosya-aguspazos.c9users.io/users/search",
+            async: false,
+            contentType: "application/json",
+             data: JSON.stringify(myObject),
+            success: function (response) {
+			    $("#suggesstion-box").empty();
+
+			    $("#shipment_receiver_email").css("background","#FFF");
+			    for(var i = 0;i<response.length;i++){
+			        var user = response[i];
+    			    if(user.email !== undefined){
+        			    $("#suggesstion-box").show();
+        			    $("#suggesstion-box").append("<div class='suggesstion'>" + user.email + "</div>");
+    			        $('.suggesstion').on('click',function(e){
+                            setMail($(this).html());
+                        });
+    			    }
+			    }
+
+            }
+    });
+};
+
+var setMail = function(text){
+    $("#shipment_receiver_email").val(text);
+    $("#suggesstion-box").hide();
+
+    
+};
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
