b011ed5916f6750c65a0fb817d56c797336d8904
Martin Long <martin.long.gz@gmail.com>
2017-10-28 03:47:02 +0000

    Merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028024703) do
+ActiveRecord::Schema.define(version: 20171028025251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,17 +95,12 @@ ActiveRecord::Schema.define(version: 20171028024703) do
     t.string "lastName"
     t.string "email"
     t.string "document"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string "provider"
-    t.string "uid"
-    t.string "oauth_token"
-    t.datetime "oauth_expires_at"
     t.string "password"
   end
 
