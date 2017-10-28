1f574931b936b0fef562473884c50e740b751651
Mathias Gili <mathias.gilik@gmail.com>
2017-10-28 03:32:04 +0000

    add license to cadets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026014542) do
+ActiveRecord::Schema.define(version: 20171028024703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20171026014542) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "license_file_name"
+    t.string "license_content_type"
+    t.integer "license_file_size"
+    t.datetime "license_updated_at"
   end
 
   create_table "images", force: :cascade do |t|
